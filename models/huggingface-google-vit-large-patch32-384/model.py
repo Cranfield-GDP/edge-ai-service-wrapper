245439065a926849cad54f0d81a658b34736d98d
ntutangyun <ntutangyun@gmail.com>
2025-04-09 12:17:21 +0100

    huggingface google vit large patch 32 384 wrap up

--- models/huggingface-google-vit-large-patch32-384/model.py
+++ models/huggingface-google-vit-large-patch32-384/model.py
@@ -11,7 +11,7 @@ from torch.profiler import profile, record_function
 import torch
 from fastapi import APIRouter, File, Form, UploadFile
 from fastapi.responses import JSONResponse
-from transformers import ViTFeatureExtractor, ViTForImageClassification
+from transformers import ViTImageProcessor, ViTForImageClassification
 from PIL import Image
 
 # --------------------------------
@@ -19,7 +19,7 @@ from PIL import Image
 # make sure the variables `MODEL_NAME` and `model` are defined here.
 # --------------------------------
 MODEL_NAME = "google/vit-large-patch32-384"
-feature_extractor = ViTFeatureExtractor.from_pretrained(MODEL_NAME)
+processor = ViTImageProcessor.from_pretrained(MODEL_NAME)
 model = ViTForImageClassification.from_pretrained(MODEL_NAME)
 model.eval()
 
@@ -33,7 +33,7 @@ async def run_model(file: UploadFile = File(...), ue_id: str = Form(...)):
     try:
         # Prepare the model input
         image = Image.open(file.file).convert("RGB")
-        inputs = feature_extractor(images=image, return_tensors="pt")
+        inputs = processor(images=image, return_tensors="pt")
 
         # Perform inference
         with torch.no_grad():
@@ -64,7 +64,7 @@ async def profile_run(file: UploadFile = File(...), ue_id: str = Form(...)):
     try:
         # Prepare the model input
         image = Image.open(file.file).convert("RGB")
-        inputs = feature_extractor(images=image, return_tensors="pt")
+        inputs = processor(images=image, return_tensors="pt")
 
         # perform profiling
         with profile(
